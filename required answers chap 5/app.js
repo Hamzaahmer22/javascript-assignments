4c16c60290f29d81d46c2d5cdbefeaed892a8f98
Hamzaahmer22 <99084597+Hamzaahmer22@users.noreply.github.com>
2024-03-02 02:39:32 +0500

    chap 5 required questions

---
+++ required answers chap 5/app.js
@@ -0,0 +1,83 @@
+
+// ADDITION
+
+var add1=25
+var add2=10
+
+var addition= add1+add2
+
+console.log(addition)
+
+
+// SUBTRACTIOn
+
+var sub1=65
+var sub2=10
+
+var subtraction = sub1-sub2
+console.log(subtraction)
+
+// MULTIPLICATION
+var mul1=20
+var mul2=5
+
+var multiplication=(mul1*mul2)
+console.log(multiplication)
+
+// DIVISION
+var div1=15
+var div2 =3
+
+var divison=(div1/div2) 
+console.log(divison)
+
+// MODULUS
+var modu1 =63
+var modu2 =5
+
+var modulus =(modu1 % modu2)
+console.log (modulus)
+
+// EXPONENTIAL
+var exp1=20
+var expo2=2
+
+var exponent=(exp1**expo2)
+console.log(exponent)
+
+
+// PERCENTAGE``
+var ObtainedMarks=359
+var outof=450
+var multiply
+
+var percentage=(329/450*100)
+console.log(percentage)
+
+// POST INCREMENT
+var Pnum=20
+var PnumAfter=Pnum++
+
+console.log(Pnum)
+console.log(PnumAfter)
+
+
+// POST DECREMENT
+var Pdnum=15
+var Pdnumafter =Pdnum--
+
+console.log(Pdnum)
+console.log(Pdnumafter)
+
+// PRE INCREMENT
+var prenum=66
+var prenumafter=++prenum
+
+console.log(prenum)
+console.log(prenumafter)
+// PRE DECREMENT
+var prenum1=30
+var prenum1after=--prenum1
+
+console.log(prenum1)
+console.log(prenum1after)
